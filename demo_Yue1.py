bb0f69675072aba565898ad753973cdd7381c841
Yue <97970714+Yue-stat@users.noreply.github.com>
2023-05-19 13:46:09 +0800

    Create demo_Yue1.py

---
+++ demo_Yue1.py
@@ -0,0 +1,58 @@
+# Yue Liu
+import random
+
+import pandas as pd
+import streamlit as st
+
+st.title("GPT Prompt and Answers demo")
+st.write(
+    "In order to prevent privacy leakage of the data we trained on, we fix the clinical notes and instructions that you can test. Please choose each of the notes and instructions!"
+)
+
+st.markdown("""---""")
+
+note_number = st.slider(
+    "Select the clinical note. There are total 10.", 1, 4, 1
+)
+
+mtsamples = pd.read_json("tmp5(4).json")
+masked = mtsamples["data"][0]
+note = masked["paragraphs"][0]['context']
+
+q_list = []
+for k, qa in enumerate(masked["paragraphs"][0]['qas']):
+    q_list.append(qa['question'])
+   
+st.info(note)
+
+instruction = st.selectbox("Select your instruction", q_list)
+
+def find_loc(string, list):
+    for i in range(len(list)):
+        if string in list[i]:
+            return i
+          
+          
+
+index = find_loc(instruction, q_list)
+Azure = ""
+for answer in masked["paragraphs"][0]['qas'][index]['answers']:
+     Azure += answer['text']
+Azure
+gpt4 = "To add"
+
+st.write("\n")
+st.markdown("""---""")
+st.write("\n")
+
+st.write("**Azure**")
+st.success(Azure)
+
+st.write("**GPT-4**")
+st.warning(gpt4)
+
+# st.write("**Alpaca**")
+# st.error(alpaca)
+
+
+
