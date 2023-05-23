85517316be5cbb5a4d4bde23296272ec9843e8e0
Yue <97970714+Yue-stat@users.noreply.github.com>
2023-05-23 13:59:06 +0800

    Update demo_Yue2.py

--- demo_Yue2.py
+++ demo_Yue2.py
@@ -78,11 +78,11 @@ for k, qa in enumerate(masked["paragraphs"][0]['qas']):
       st.markdown("""---""")
   
     with col3:
-        globals()["A" + str(30*(note_number-1)+k)] = st.radio(
+        A60 = st.radio(
             "Choose an answer 👇",
             ("ChatGPT", "GPT-4"),
     )
-        eval("A" + str(30*(note_number-1)+k))
-        A[note_number-1][k] = eval("A" + str(30*(note_number-1)+k))
+        A60
+        A[note_number-1][k] = A60
   
 
