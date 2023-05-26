df58d906f4bfc46a01d50fc85d6a382242137db0
Yue <97970714+Yue-stat@users.noreply.github.com>
2023-05-26 21:36:57 +0800

    Update demo_Yue2.py

--- demo_Yue2.py
+++ demo_Yue2.py
@@ -142,8 +142,8 @@ for k, qa in enumerate(masked["paragraphs"][0]['qas']):
     Azure = Azure[2:]
     
     gpt4 = "To add"
-    if k <= 7:
-        gpt4 = gpt4_result[note_number-1][k]
+    # if k <= 7:
+        # gpt4 = gpt4_result[note_number-1][k]
         
     col1, col2, col3 = st.columns([1.5,2,0.7])
     with col1:
