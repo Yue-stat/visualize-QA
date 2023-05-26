165fc02623a034d86f49114da06110ac24588a8b
Yue <97970714+Yue-stat@users.noreply.github.com>
2023-05-26 20:35:28 +0800

    Update demo_Yue2.py

--- demo_Yue2.py
+++ demo_Yue2.py
@@ -147,8 +147,10 @@ for k, qa in enumerate(masked["paragraphs"][0]['qas']):
         
     col1, col2, col3 = st.columns([1.5,2,0.7])
     with col1:
-      st.write("Q" + qa['id'] + qa['question'])
-      st.markdown("""---""")
+        qa['id']
+        st.write("Q" + qa['id'] + qa['question'])
+        
+        st.markdown("""---""")
           
           
     with col2:
